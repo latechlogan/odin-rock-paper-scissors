481812c91a5e17dee89873e1454b72e85a5a6e70
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-07-25 15:57:46 -0500

    Add rock, paper, scissors pseudocode to script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+//  Collect and store user input (rock, paper, or scissors)
+//  Create computer input
+//      Store rock, paper, scissors in array
+//      Generate random number 0, 1, 2
+//      Get value (rock, paper, scissors) at index (random number) of array
+//  Compare user input to computer input
+//      if else accounting for user and computer inputs
+//  Alert the winner
