e975137436251aaec06d7a7fd0a8a6568fa49cd4
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-07-25 16:46:16 -0500

    Create comp selection and refactor user selection

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 //  Collect and store user input (rock, paper, or scissors)
-document.addEventListener("click", (event) => {
-  let userSelection = event.target.value;
-  return userSelection;
+document.addEventListener("click", (e) => {
+  let userSelection = getUserSelection(e.target);
+  console.log(userSelection);
 });
+
+function getUserSelection(target) {
+  return target.value;
+}
 //  Create computer input
 //      Store rock, paper, scissors in array
+const CHOICES = ["rock", "paper", "scissors"];
 //      Generate random number 0, 1, 2
 //      Get value (rock, paper, scissors) at index (random number) of array
+function getCompSelection() {
+  let i = Math.floor(Math.random() * 3);
+  return CHOICES[i];
+}
 //  Compare user input to computer input
 //      if else accounting for user and computer inputs
 //  Alert the winner
